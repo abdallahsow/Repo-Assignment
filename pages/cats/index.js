999b82e3e5b2ce1e08e8fa36e5332b7f368a1d13
Abdallah Sow <abdallahsow700@gmail.com>
2022-11-06 12:33:29 +0300

    fetch the data in the cats page

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import Navbar from "../../components/Navbar/Navbar";
 
 const cats = function () {
+  const [cats, setCats] = useState([]);
+
+  const catsData = async () => {
+    const response = await fetch("/api/cats");
+    const json = await response.json();
+    setCats(json);
+  };
+
+  useEffect(() => {
+    catsData();
+  }, []);
+
+  console.log(cats);
+
   return (
     <div>
       <Navbar />
-      <Card />
+      {cats.map((cat, index) => (
+        <Card key={index} {...cat} />
+      ))}
     </div>
   );
 };
