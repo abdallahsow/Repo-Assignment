c70432492d46d43b818a4495ef280a5f42493ba8
Abdallah Sow <abdallahsow700@gmail.com>
2022-11-07 12:10:11 +0300

    created the layout folder

---
+++ layouts/Default.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar/Navbar";
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default DefaultLayout;
