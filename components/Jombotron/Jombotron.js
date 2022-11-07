5896fe64b8f0f3d2600b25bad1b4cabaa19f6929
Abdallah Sow <abdallahsow700@gmail.com>
2022-11-07 12:05:27 +0300

    refactored the code using Axios

--- components/Jombotron/Jombotron.js
+++ components/Jombotron/Jombotron.js
@@ -4,10 +4,10 @@ const Jombotron = () => {
   const router = useRouter();
 
   return (
-    <div class="jumbotron jumbotron-fluid mt-5">
-      <div class="container">
-        <h1 class="display-4">Cats World</h1>
-        <p class="lead">
+    <div className="jumbotron jumbotron-fluid mt-5">
+      <div className="container">
+        <h1 className="display-4">Cats World</h1>
+        <p className="lead">
           Find your new best friend today. Adopt a wonderful cat.
         </p>
         <button
--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Axios from "axios";
 
 export default function Cat() {
   const [cat, setCat] = useState(null);
@@ -8,29 +9,27 @@ export default function Cat() {
   const { id } = router.query;
 
   const fetchCat = async () => {
-    const response = await fetch(`/api/cats/${id}`);
-    const data = response.json();
-    setCat(data);
+    const response = await Axios.get(`/api/cats/${id}`);
+    setCat(response.data);
   };
 
   useEffect(function () {
     fetchCat();
   }, []);
 
-  console.log(cat);
-
   return (
-    // <div>
-    //   {cat.length && (
-    //     <div className="container mt-5" style={{ display: "flex" }}>
-    //       <img src={cat.image.url} alt={cat.image.alt} />
-    //       <div className="mx-5">
-    //         <h1>{cat.name}</h1>
-    //         <p>{cat.description}</p>
-    //       </div>
-    //     </div>
-    //   )}
-    // </div>
-    <pre>{JSON.stringify(cat, null, 2)}</pre>
+    <>
+      <div>
+        {cat && (
+          <div className="container mt-5" style={{ display: "flex" }}>
+            <img src={cat.image.url} alt={cat.image.alt} />
+            <div className="mx-5">
+              <h1>{cat.name}</h1>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
